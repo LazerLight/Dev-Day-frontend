55421b12abfd9192c3f7f74ad180a6a99b691865
Rod <greasytaco@live.com>
2018-05-16 14:52:03 +0200

    heroku

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { SignupCredentials, UserService } from "../api/user.service";
 import { Router } from "@angular/router";
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/environment.prod";
 
 @Component({
   selector: "app-signup",
