68cecc03c64fb9e477508b58ce75dde77b174b1c
Rod <greasytaco@live.com>
2018-05-17 11:45:00 +0200

    added a modal in one-board html

--- src/app/app-materialize.module.ts
+++ src/app/app-materialize.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MzButtonModule, MzNavbarModule, MzCollectionModule, MzCollapsibleModule, MzInputModule} from 'ng2-materialize'
+import { MzButtonModule, MzNavbarModule, MzCollectionModule, MzCollapsibleModule, MzInputModule, MzModalModule} from 'ng2-materialize'
 
 const mat_modules = [
     MzNavbarModule,
@@ -8,6 +8,7 @@ const mat_modules = [
     MzCollectionModule,
     MzCollapsibleModule,
     MzInputModule,
+    MzModalModule
 ]
 @NgModule({
     imports: [
